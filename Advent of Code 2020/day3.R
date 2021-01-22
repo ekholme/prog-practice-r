9b36985feceba55745821c8d3efb1b2e15a37808
Eric Ekholm <eric.ekholm@gmail.com>
2021-01-21 21:46:35 -0500

    day 3 part 2

--- Advent of Code 2020/day3.R
+++ Advent of Code 2020/day3.R
@@ -48,5 +48,47 @@ answer
 
 # Part 2 ------------------------------------------------------------------
 
+#writing a function to figure out which position we need to check
+get_pos <- function(x, mult) {
+  (x-1)*mult + 1
+}
 
-  
+#realizing I need to extend my extended_vec even more
+longer_vec <- map_chr(use_vec, ~paste(rep(.x, 73), collapse = ""))
+
+#creating a df to figure out the positions to check against for each case
+pos_df <- map_dfc(c(1, 3, 5, 7), ~get_pos(x = 1:length(extended_vec), mult = .x)) %>%
+  slice(-1) #removing the top row bc it's our starting position
+
+names(pos_df) <- c("pos1", "pos3", "pos5", "pos7")
+
+#this gets our answers for all except the 'right 1 down 2' scenario
+trees_df <- map_dfc(pos_df,
+           ~map2_lgl(
+             longer_vec[2:323],
+             .,
+             is_tree
+           ))
+
+
+#essentially the logic here is to get the odd numbered indices from the longer_vec
+#for the 'down 2' scenario
+keeps <- seq(1, 323, by = 2)
+twos_vec <- longer_vec[keeps]
+
+#now i can just repeat the process from earlier
+pos_twos <- get_pos(x = 1:length(twos_vec), mult = 1)
+
+twos_tmp <- map2_lgl(twos_vec[2:length(twos_vec)],
+                     pos_twos[2:length(pos_twos)],
+                     is_tree)
+
+twos_sum <- sum(twos_tmp)
+
+#getting final answer
+part_two_answer <- trees_df %>%
+  summarize(across(everything(), sum)) %>%
+  t() %>%
+  as.vector() %>%
+  append(twos_sum) %>%
+  reduce(`*`, .init = 1)
